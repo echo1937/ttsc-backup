a08c91cc430c219799cb5d1221d61c79744806a1
rMBP13 <numlock178@gmail.com>
2017-02-09 14:22:49 +0800

    终于解决了@value的问题, 原因在于applicationContext-dao.xml文件中的context:property-placeholder扫描标签只能使用一次(多次使用后续不会被读取, 定义的属性在发生注入时自然也就会找不到). 另外properties文件中都是使用域名, 因此部署的主机需要配置dns解析.

--- taotao-manager/taotao-manager-service/src/main/java/com/taotao/service/impl/PictureServiceImpl.java
+++ taotao-manager/taotao-manager-service/src/main/java/com/taotao/service/impl/PictureServiceImpl.java
@@ -4,6 +4,7 @@ import com.taotao.common.utils.FtpUtil;
 import com.taotao.common.utils.IDUtils;
 import com.taotao.service.PictureService;
 import org.joda.time.DateTime;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -14,31 +15,31 @@ import java.util.Map;
 @Service
 public class PictureServiceImpl implements PictureService {
 
-//    @Value("${FTP_ADDRESS}")
-//    private String FTP_ADDRESS;
-//
-//    @Value("${FTP_PORT}")
-//    private Integer FTP_PORT;
-//
-//    @Value("${FTP_USERNAME}")
-//    private String FTP_USERNAME;
-//
-//    @Value("${FTP_PASSWORD}")
-//    private String FTP_PASSWORD;
-//
-//    @Value("${FTP_BASE_PATH}")
-//    private String FTP_BASE_PATH;
-//
-//    @Value("${IMAGE_BASE_URL}")
-//    private String IMAGE_BASE_URL;
+    @Value("${FTP_ADDRESS}")
+    private String FTP_ADDRESS;
+
+    @Value("${FTP_PORT}")
+    private Integer FTP_PORT;
+
+    @Value("${FTP_USERNAME}")
+    private String FTP_USERNAME;
+
+    @Value("${FTP_PASSWORD}")
+    private String FTP_PASSWORD;
+
+    @Value("${FTP_BASE_PATH}")
+    private String FTP_BASE_PATH;
+
+    @Value("${IMAGE_BASE_URL}")
+    private String IMAGE_BASE_URL;
 
 
-    private String FTP_ADDRESS = "172.28.128.5";
-    private Integer FTP_PORT = 21;
-    private String FTP_USERNAME = "ttsc";
-    private String FTP_PASSWORD = "redhat";
-    private String FTP_BASE_PATH = "/usr/share/nginx/html/images";
-    private String IMAGE_BASE_URL = "http://172.28.128.5/images";
+    // private String FTP_ADDRESS = "172.28.128.5";
+    // private Integer FTP_PORT = 21;
+    // private String FTP_USERNAME = "ttsc";
+    // private String FTP_PASSWORD = "redhat";
+    // private String FTP_BASE_PATH = "/usr/share/nginx/html/images";
+    // private String IMAGE_BASE_URL = "http://172.28.128.5/images";
 
 
     @Override
