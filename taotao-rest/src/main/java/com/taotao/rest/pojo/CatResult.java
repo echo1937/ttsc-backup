f78cf4809d2cd7cce32086317147d2acad7e38bc
rMBP13 <numlock178@gmail.com>
2017-02-14 14:07:17 +0800

    第五天: jsonp远程调用, 商品分类展示服务-Service层, 商品分类展示-完成

---
+++ taotao-rest/src/main/java/com/taotao/rest/pojo/CatResult.java
@@ -0,0 +1,18 @@
+package com.taotao.rest.pojo;
+
+import java.util.List;
+
+/**
+ * Created by Eric on 2/14/17.
+ */
+public class CatResult {
+    private List<?> data;
+
+    public List<?> getData() {
+        return data;
+    }
+
+    public void setData(List<?> data) {
+        this.data = data;
+    }
+}
---
+++ taotao-rest/src/main/java/com/taotao/rest/service/ItemCatService.java
@@ -0,0 +1,10 @@
+package com.taotao.rest.service;
+
+import com.taotao.rest.pojo.CatResult;
+
+/**
+ * Created by Eric on 2/14/17.
+ */
+public interface ItemCatService {
+    CatResult getItemCatList();
+}
