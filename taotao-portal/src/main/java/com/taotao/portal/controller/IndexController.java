56f307cbeebe914cc9a8a14a68cd772ad51d5edc
rMBP13 <numlock178@gmail.com>
2017-02-15 23:39:38 +0800

    第6天: 11 - 14 (11. httpclient介绍, 执行get请求, 执行post请求, 首页大广告位展示)

--- taotao-portal/src/main/java/com/taotao/portal/controller/IndexController.java
+++ taotao-portal/src/main/java/com/taotao/portal/controller/IndexController.java
@@ -1,15 +1,34 @@
 package com.taotao.portal.controller;
 
+import com.taotao.portal.service.ContentService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Created by Eric on 2/13/17.
  */
 @Controller
 public class IndexController {
+
+    @Autowired
+    private ContentService contentService;
+
     @RequestMapping("/index")
-    public String showIndex() {
+    public String showIndex(Model model) {
+        String adJson = contentService.getContentList();
+        model.addAttribute("ad1", adJson);
         return "index";
+    }
+
+    @RequestMapping(value = "/httpclient/post", method = RequestMethod.POST)
+    @ResponseBody
+    public String testPost(@RequestParam(defaultValue = "ABC") String username,
+                           @RequestParam(defaultValue = "EFG") String password) {
+        return "username:" + username + "\tpassword:" + password;
     }
 }
---
+++ taotao-portal/src/main/java/com/taotao/portal/service/ContentService.java
@@ -0,0 +1,8 @@
+package com.taotao.portal.service;
+
+/**
+ * Created by Eric on 2/15/17.
+ */
+public interface ContentService {
+    String getContentList();
+}
