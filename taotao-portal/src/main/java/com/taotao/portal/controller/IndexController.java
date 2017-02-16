81b3d923da82bb26f5104a94ad7c030291774922
rMBP13 <numlock178@gmail.com>
2017-02-16 14:07:19 +0800

    解决了 第六天: httpclient的Post测试中出现的406错误和返回字符串乱码问题.

--- taotao-portal/src/main/java/com/taotao/portal/controller/IndexController.java
+++ taotao-portal/src/main/java/com/taotao/portal/controller/IndexController.java
@@ -1,7 +1,9 @@
 package com.taotao.portal.controller;
 
+import com.taotao.common.pojo.TaotaoResult;
 import com.taotao.portal.service.ContentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,10 +27,24 @@ public class IndexController {
         return "index";
     }
 
+    // 该处理器居然可以和http://localhost:8082/httpclient/post.action相匹配
     @RequestMapping(value = "/httpclient/post", method = RequestMethod.POST)
     @ResponseBody
-    public String testPost(@RequestParam(defaultValue = "ABC") String username,
-                           @RequestParam(defaultValue = "EFG") String password) {
-        return "username:" + username + "\tpassword:" + password;
+    public TaotaoResult testPost() {
+        return TaotaoResult.ok();
+    }
+
+    // 该处理器居然可以和http://localhost:8082/httpclient/post1.action相匹配
+    // produces="xxxxx"可以让return result返回的字符串不再出现乱码
+    @RequestMapping(value = "/httpclient/post1", method = RequestMethod.POST,
+            produces = MediaType.TEXT_PLAIN_VALUE + ";charset=utf-8")
+    @ResponseBody
+    public String testPost1(@RequestParam(defaultValue = "ABC") String username,
+                            @RequestParam(defaultValue = "EFG") String password) {
+        String result = "username:" + username + ",password:" + password;
+        // 服务端打印result并不是乱码
+        System.out.println(result);
+        // 浏览器获取的json却会导致中文输出乱码
+        return result;
     }
 }
